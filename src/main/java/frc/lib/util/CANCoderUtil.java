b0e3decb1ca50efb5f7d88fd9256fccf5f6c1f8a
GregoryLi360 <gregoryli360@gmail.com>
2024-01-10 15:30:04 -0500

    update to newest Phoenix6 version

--- src/main/java/frc/lib/util/CANCoderUtil.java
+++ src/main/java/frc/lib/util/CANCoderUtil.java
@@ -1,7 +1,7 @@
 package frc.lib.util;
 
-import com.ctre.phoenix.sensors.CANCoder;
-import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.sensors.CANCoderStatusFrame;
 
 /** Sets status frames for the CTRE CANCoder. */
 public class CANCoderUtil {
@@ -23,8 +23,9 @@ public class CANCoderUtil {
    * @param usage The status frame feedback to enable. kAll is the default when a CANCoder
    *     isconstructed.
    */
-  public static void setCANCoderBusUsage(CANCoder cancoder, CCUsage usage) {
+  public static void setCANCoderBusUsage(CANcoder cancoder, CCUsage usage) {
     if (usage == CCUsage.kAll) {
+      cancoder.
       cancoder.setStatusFramePeriod(CANCoderStatusFrame.SensorData, 10);
       cancoder.setStatusFramePeriod(CANCoderStatusFrame.VbatAndFaults, 10);
     } else if (usage == CCUsage.kSensorDataOnly) {

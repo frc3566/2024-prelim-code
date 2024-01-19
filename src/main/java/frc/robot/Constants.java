3050c20e94a2d807506c2c7828a9d4e09ed65975
GregoryLi360 <gregoryli360@gmail.com>
2024-01-19 16:58:59 -0500

    Clean up code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,10 +127,11 @@ public final class Constants {
           canCoderID, angleOffset);
     }
   }
+
   public static class Shooter {
       public static int Left_Motor_Id = 10;
       public static int Right_Motor_Id = 9;
-    }
+  }
 
   public static final class AutoConstants {
     public static final double kMaxSpeedMetersPerSecond = 3;

7bae6f348d8e53b841c07e8bf0b58b9d41802b48
GregoryLi360 <gregoryli360@gmail.com>
2024-01-22 12:02:12 -0500

    feat: completed sysid code todo: test with robot

--- src/main/java/frc/robot/subsystems/SysIdSwerve.java
+++ src/main/java/frc/robot/subsystems/SysIdSwerve.java
@@ -1,58 +1,78 @@
 package frc.robot.subsystems;
 
+import java.util.Arrays;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.SwerveControlRequestParameters;
+
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Velocity;
 import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.SwerveVoltageRequest;
 
 public class SysIdSwerve extends Swerve {
-    private final SysIdRoutine m_sysIdRoutine = new SysIdRoutine(
-        // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
-        new SysIdRoutine.Config(),
-        new SysIdRoutine.Mechanism(
-            // Tell SysId how to plumb the driving voltage to the motors.
-            (Measure<Voltage> volts) -> {
-                final MutableMeasure<Voltage> voltage = MutableMeasure.mutable(Units.Volts.of(0));
-                mSwerveMods[0].getDriveMotor().setVoltage(voltage.in(Units.Volts));
-                mSwerveMods[1].getDriveMotor().setVoltage(voltage.in(Units.Volts));
-                mSwerveMods[2].getDriveMotor().setVoltage(voltage.in(Units.Volts));
-                mSwerveMods[3].getDriveMotor().setVoltage(voltage.in(Units.Volts));
-            },
-            // Tell SysId how to record a frame of data for each motor on the mechanism being characterized.
-            log -> { 
-                // // Record a frame for the left motors.  Since these share an encoder, we consider the entire group to be one motor.
-                // log.motor("drive-left") 
-                //     .voltage(
-                //         m_appliedVoltage.mut_replace(
-                //             m_leftMotor.get() * RobotController.getBatteryVoltage(), Volts))
-                //     .linearPosition(m_distance.mut_replace(m_leftEncoder.getDistance(), Meters))
-                //     .linearVelocity(
-                //         m_velocity.mut_replace(m_leftEncoder.getRate(), MetersPerSecond));
-                
-                // // Record a frame for the right motors.  Since these share an encoder, we consider the entire group to be one motor.
-                // log.motor("drive-right")
-                //     .voltage(
-                //         m_appliedVoltage.mut_replace(
-                //             m_rightMotor.get() * RobotController.getBatteryVoltage(), Volts))
-                //     .linearPosition(m_distance.mut_replace(m_rightEncoder.getDistance(), Meters))
-                //     .linearVelocity(
-                //         m_velocity.mut_replace(m_rightEncoder.getRate(), MetersPerSecond));
-            },
-            
-            // Tell SysId to make generated commands require this subsystem, suffix test state in
-            // WPILog with this subsystem's name ("drive")
-            this
-        )
+    /* Mutable Measures to keep track of */
+    private final MutableMeasure<Voltage> appliedVoltage = MutableMeasure.mutable(Units.Volts.of(0));
+    private final MutableMeasure<Distance> distance = MutableMeasure.mutable(Units.Meters.of(0));
+    private final MutableMeasure<Velocity<Distance>> velocity = MutableMeasure.mutable(Units.MetersPerSecond.of(0));
+    private final MutableMeasure<Angle> angle = MutableMeasure.mutable(Units.Radians.of(0));
+    private final MutableMeasure<Velocity<Angle>> angularVelocity = MutableMeasure.mutable(Units.RotationsPerSecond.of(0));
+
+    /* Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage. */
+    private final SysIdRoutine.Config routineConfig = new SysIdRoutine.Config();
+
+    private final SysIdRoutine.Mechanism routineMechanism = new SysIdRoutine.Mechanism(
+        this::routineDriving,
+        this::routineLogging,
+        /* Tell SysId to make the generated commands require this subsystem, 
+            suffix test state in WPILog with this subsystem's name ("drive") */
+        this 
     );
 
+    /* Tell SysId how to plumb the driving voltage to the motors. */
+    private void routineDriving(Measure<Voltage> volts) {
+        appliedVoltage.mut_replace(volts);
+        new SwerveVoltageRequest()
+            .withVoltage(volts.in(Units.Volts))
+            .apply(new SwerveControlRequestParameters(
+
+            ), mSwerveMods);
+    }
+
+    /* Tell SysId how to record a frame of data for each motor on the mechanism being characterized. */
+    private void routineLogging(SysIdRoutineLog log) {
+        /* Record a frame for every module. 
+            Since each motor in a module shares an encoder, we consider the entire group to be one motor. */
+
+        Arrays.asList(mSwerveMods).forEach(e -> {
+            log.motor("Motor " + e.moduleNumber)
+                .voltage(appliedVoltage)
+                .linearPosition(distance.mut_replace(e.getPosition().distanceMeters, Units.Meters))
+                .linearVelocity(velocity.mut_replace(e.getState().speedMetersPerSecond, Units.MetersPerSecond))
+                .angularPosition(angle.mut_replace(e.getPosition().angle.getRadians(), Units.Radians))
+                .angularVelocity(angularVelocity.mut_replace(e.getAngleSpeed(), Units.RotationsPerSecond));
+        });
+    }
+
+    public SysIdRoutine getRoutine() {
+        return new SysIdRoutine(
+            routineConfig,
+            routineMechanism
+        );
+    }
+
     public Command sysIdQuasistatic(Direction kforward) {
-        throw new UnsupportedOperationException("Unimplemented method 'sysIdQuasistatic'");
+        return getRoutine().quasistatic(kforward);
     }
 
     public Command sysIdDynamic(Direction kforward) {
-        throw new UnsupportedOperationException("Unimplemented method 'sysIdDynamic'");
+        return getRoutine().dynamic(kforward);
     }
 }

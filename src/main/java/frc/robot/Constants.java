43c759c3f5093953a27696e4bf1f57bbf8916214
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2024-01-24 14:57:59 -0500

    Module 1 drive motor inversion

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public final class Constants {
       public static final int driveMotorID = 3;
       public static final int angleMotorID = 4;
       public static final int canCoderID = 1;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(200.65);
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(20.65);
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
     }

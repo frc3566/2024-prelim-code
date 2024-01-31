a0961b1c676c77ced6fbd00e099c425b4f1dc175
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2024-01-31 15:07:31 -0500

    Shoot PID done

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -3,20 +3,26 @@ package frc.robot.commands;
 import frc.robot.subsystems.*;
 
 import java.util.function.DoubleSupplier;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Shoot extends Command {
     private Shooter s_Shooter;
-    private DoubleSupplier left, right;
+    private DoubleSupplier left, right, targetVelocity;
 
     public Shoot(Shooter s_Shooter, DoubleSupplier leftTrigger, DoubleSupplier rightTrigger) {
+    // public Shoot(Shooter s_Shooter, DoubleSupplier leftTrigger, DoubleSupplier rightTrigger, DoubleSupplier targetVelocity) {
         addRequirements(s_Shooter);
         this.s_Shooter = s_Shooter;
         this.left = leftTrigger;
         this.right = rightTrigger;
+        // this.targetVelocity = targetVelocity;
     }
     
     public void execute() {
+        // SparkPIDController pid = s_Shooter.PIDController;
+        // pid.setReference(targetVelocity, ControlType.kVelocity);
         s_Shooter.setPower(left.getAsDouble() - right.getAsDouble());
     }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,12 +4,14 @@ import frc.robot.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkPIDController;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
 
     public CANSparkMax left, right;
-    public double ltrigger, rtrigger;
+    public double ltrigger, rtrigger, currentSpeed, targetSpeed;
     public SparkPIDController PIDController;
     public Shooter() {
         left = new CANSparkMax(Constants.Shooter.Left_Motor_Id, MotorType.kBrushless);
@@ -18,10 +20,16 @@ public class Shooter extends SubsystemBase {
         right.setInverted(true);
         left.setSmartCurrentLimit(20);
         right.setSmartCurrentLimit(20);
+
+
+
         PIDController = left.getPIDController();
         PIDController.setP(Constants.Shooter.ShooterKP);
         PIDController.setD(Constants.Shooter.ShooterKD);
         PIDController.setI(Constants.Shooter.ShooterKI);
+
+        
+    
     }
  
     public void setPower(double power) {
@@ -29,16 +37,6 @@ public class Shooter extends SubsystemBase {
         right.set(power);
     }
 
-    // public void setVoltage(double voltage) {
-    //     left.setVoltage(voltage);
-    //     right.setVoltage(voltage);
-    // }
-
-    // public void setBrake(boolean isBrake) {
-    //     IdleMode sparkMode = isBrake ? IdleMode.kBrake : IdleMode.kCoast;
-    //     left.setIdleMode(sparkMode);
-    //     right.setIdleMode(sparkMode);
-    // }
 
     public void stop() {
         left.stopMotor();

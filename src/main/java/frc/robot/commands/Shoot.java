5008ef44f30fbc10ceef8c7e5e0ebf9837a93e3d
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2024-02-05 15:52:08 -0500

    Updated shooter pid code

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -9,19 +9,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class Shoot extends Command {
     private Shooter s_Shooter;
-    private double targetVelocity;
-    private SparkPIDController pid;
+    private double targetVelocity = 0.5;
 
-    public Shoot(Shooter s_Shooter, double targetVelocity) {
-    // public Shoot(Shooter s_Shooter, DoubleSupplier leftTrigger, DoubleSupplier rightTrigger, DoubleSupplier targetVelocity) {
+    public Shoot(Shooter s_Shooter) {
+    // public Shoot(Shooter s_Shooter, DoubleSupplier targetVelocity) {
         addRequirements(s_Shooter);
         this.s_Shooter = s_Shooter;
-        pid = s_Shooter.PIDController;
-        // pid.setReference(-targetVelocity, ControlType.kVelocity);
+        SparkPIDController pid = s_Shooter.PIDController;
+        pid.setReference(targetVelocity, ControlType.kDutyCycle);
+
+        // this.targetVelocity = targetVelocity;
     }
     
     public void execute() {
-        pid.setReference(-targetVelocity, ControlType.kDutyCycle);
-        s_Shooter.setPower(-targetVelocity);
+        s_Shooter.setPower(targetVelocity);
     }
 }
